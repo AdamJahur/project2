e4b2da6d79d9fadb4d7421ce54ec89f04643d5c4
Devin Zimmerman <dzimmerman902@gmail.com>
2017-02-09 19:36:42 -0500

    Got the server to work

---
+++ routes/html-routes.js
@@ -0,0 +1,17 @@
+// *********************************************************************************
+// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+	app.get("/", function(req, res) {
+
+		res.render("index");
+	})
+}
--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.set("view engine", "handlebars");
 
 // Routes =============================================================
 
-// require("./routes/html-routes.js")(app);
+require("./routes/html-routes.js")(app);
 // require("./routes/post-api-routes.js")(app);
 // require("./routes/author-api-routes.js")(app);
 
