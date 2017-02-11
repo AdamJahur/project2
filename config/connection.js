90780189600770f53373bebf04ea8f88690c1473
Devin Zimmerman <dzimmerman902@gmail.com>
2017-02-11 11:14:03 -0500

    created connection.js

---
+++ config/connection.js
@@ -0,0 +1,8 @@
+var Sequelize = require('sequelize');
+
+var sequelize = new Sequelize('military_connect', 'root', '35cYqeqPam5g', {
+	host: 'localhost',
+	dialect: 'mysql'
+});
+
+module.exports = sequelize;
