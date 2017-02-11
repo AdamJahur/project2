468107b1cfd19459d8d8b4ff46eb68011a3b1839
dzimmerman902 <dzimmerman902@gmail.com>
2017-02-11 12:31:53 -0500

    Revert "Edbranch"

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 // *****************************************************************************
 // Server.js - This file is the initial starting point for the Node/Express server.
 //
-
 // ******************************************************************************
 // *** Dependencies
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
-
-var exphbs = require('express-handlebars');
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
-
 
 // Sets up the Express App
 // =============================================================
@@ -23,12 +15,6 @@ var PORT = process.env.PORT || 3000;
 // Requiring our models for syncing
 var db = require("./models");
 
-
-//handlebars link
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-
-
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -37,17 +23,6 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // Static directory
 app.use(express.static("./public"));
-
-
-// Routes =============================================================
-
-//Files not implemented yet commenting routes for now
-
-// require("./routes/html-routes.js")(app);
-// require("./routes/api-routes.js")(app);
-
-// Syncing our sequelize models and then starting our express app
-db.sequelize.sync().then(function() {
 
 // Set Handlebars
 var exphbs = require("express-handlebars");
@@ -63,16 +38,7 @@ require("./routes/html-routes.js")(app);
 
 // Syncing our sequelize models and then starting our express app
 db.sequelize.sync({ force: true }).then(function() {
-
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
 });
-
-
-
-
-app.get('/', function(req, res){
-	res.render('vetTable')
-});
-
