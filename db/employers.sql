f2420000855686662af25b6799a972388069fe3a
Adam Jahur <adamnjahur@gmail.com>
2017-02-14 18:21:53 -0500

    figuring out branch files

--- db/employers.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `employers` VALUES (1,'Dell','John','Doe','www.dell.com','407-555-5555','dell@gmail.com','1000 North Street','City','State',12345,'user1','password1','image1.png'),(2,'Addidas','Carl','Beck','www.addidas.com','407-567-5555','addidas@gmail.com','2000 South Street','Point','FL',34567,'user2','password2','image2.png');
-
