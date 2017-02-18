b2e23201fb1b70fef0ffea5795868fbafb13fb69
Devin Zimmerman <dzimmerman902@gmail.com>
2017-02-17 22:38:49 -0500

    Cleaned up jobs.handlebars

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get('/', function(req, res) {
 });
 
 // Syncing our sequelize models and then starting our express app
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({ force: false }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
